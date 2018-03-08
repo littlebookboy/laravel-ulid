440a76f4351530b0187602eda28a1ea74e667429
gene <gene@js-tech.tw>
2018-03-08 14:55:45 +0800

    add boot trait function

--- src/HasUlidTrait.php
+++ src/HasUlidTrait.php
@@ -1,12 +1,22 @@
 <?php
 namespace Rorecek\Ulid;
 
-trait HasUlid
+trait HasUlidTrait
 {
-    protected static function boot()
+    /*
+     * This function is used internally by Eloquent models to test if the model has auto increment value
+     * @returns bool Always false
+     */
+    public function getIncrementing()
     {
-        parent::boot();
+        return false;
+    }
 
+    /**
+     * This function overwrites the default boot static method of Eloquent models.
+     */
+    public static function bootHasUlidTrait()
+    {
         static::creating(function ($model) {
             if (! $model->id) {
                 $model->id = \Ulid::generate();
